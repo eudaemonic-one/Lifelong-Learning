e573f25d192fc81104f9747fdf7a8ff75a79b67e
Rong Cui <brookcui97@gmail.com>
2019-11-02 22:50:25 +0800

    [LeetCode] Add Python3 Solution for 82. Remove Duplicates from Sorted List II

---
+++ LeetCode/Python3/remove-duplicates-from-sorted-list-ii.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        dummy = prev = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
+            else:
+                prev = prev.next
+                head = head.next
+        return dummy.next
