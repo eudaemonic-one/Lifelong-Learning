ef99a2fa576d35ac34ff605f8f17ee6fa9faa97d
Rong Cui <brookcui97@gmail.com>
2019-11-20 11:20:24 +0800

    [LeetCode] Add Golang Solution for 328. Odd Even Linked List

---
+++ LeetCode/Golang/odd-even-linked-list.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+    if (head == nil || head.Next == nil) {
+        return head
+    }
+    odd := head
+    even := head.Next
+    evenHead := even
+    for even != nil && even.Next != nil {
+        odd.Next = even.Next
+        odd = odd.Next
+        even.Next = odd.Next
+        even = even.Next
+    }
+    odd.Next = evenHead
+    return head
+}
