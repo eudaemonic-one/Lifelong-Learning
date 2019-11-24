84f2c339df2de2b896a3ebaa36888546eea19b9f
Rong Cui <brookcui97@gmail.com>
2019-11-24 13:32:32 +0800

    [LeetCode] Add Golang Solution for 155. Min Stack

---
+++ LeetCode/Golang/min-stack.go
@@ -0,0 +1,49 @@
+type MinStack struct {
+    stack []int
+    minStack []int
+}
+
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+    return MinStack{
+        stack: make([]int, 0),
+        minStack: make([]int, 0),
+    }
+}
+
+
+func (this *MinStack) Push(x int)  {
+	this.stack = append(this.stack, x)
+	if len(this.minStack) == 0 || x <= this.GetMin() {
+		this.minStack = append(this.minStack, x)
+	}
+}
+
+
+func (this *MinStack) Pop()  {
+	if this.Top() == this.GetMin() {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+	this.stack = this.stack[:len(this.stack)-1]
+}
+
+
+func (this *MinStack) Top() int {
+    return this.stack[len(this.stack)-1]
+}
+
+
+func (this *MinStack) GetMin() int {
+    return this.minStack[len(this.minStack)-1]
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
